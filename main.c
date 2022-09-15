b4e8aaa91eb35ee61ae30eb7900212c946e7c658
hyper1923 <hyperamabanyiyen@gmail.com>
2022-09-15 20:17:33 +0300

    push

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 const char* HELP = "encryptor-help:\n\n--encrypt: will encrypt your text\n--decrypt: will decrypt your text\n";
-
+unsigned int seed = 300; //DEFAULT SEED
 
 char* decrypt(const char* text);
 char* encrypt(const char* text);
@@ -12,7 +12,7 @@ char* decrypt(const char* text){
     unsigned int lenght = strlen(text);
     char* res = (char*)malloc(lenght);
     for(unsigned int i = 0; i < lenght; i++){
-        res[i] = ((i % 2) == 0) ? (char)((int)text[i]-2) : (char)((int)text[i]-1);
+        res[i] = ((i % 2) == 0) ? (char)((int)text[i]-(rand() % 15)) : (char)((int)text[i]- (rand() % 5));
     }
     res[lenght] = '\0';
     return res;
@@ -22,7 +22,7 @@ char* encrypt(const char *text){
     unsigned int lenght = strlen(text);
     char* res = (char*)malloc(lenght+1);
     for(unsigned int i = 0; i < lenght; i++){
-        res[i] = ((i % 2) == 0) ? (char)((int)text[i]+2) : (char)((int)text[i]+1);
+        res[i] = ((i % 2) == 0) ? (char)((int)text[i]+(rand() % 15)) : (char)((int)text[i]+(rand() % 5));
     }
     res[lenght] = '\0';
     return res;
@@ -31,6 +31,8 @@ char* encrypt(const char *text){
 int main(int argc, char** argv){
     unsigned char used = 0;
     for(unsigned int i = 0; i < argc; i++){
+        if(!strcmp(argv[i], "--seed"))
+        {srand(atoi(argv[(i+1)])); i++;}
         if(!strcmp(argv[i], "--encrypt"))
         {char* d = argv[(i+1)]; printf("Encrypted: %s\n\r", encrypt(d));i++; used = 1;};
         if(!strcmp(argv[i], "--decrypt"))
